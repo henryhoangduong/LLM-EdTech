546f6572e29dca598205bb7c477f3cdd48090c7e
henryhoangduong <henryhoangduong@gmail.com>
2025-09-29 05:44:30 +0700

    frontend: react pdf viewer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import App from './App.tsx'
 import '@/styles/globals.css'
 import QueryProvider from './context/query-provider.tsx'
 import { AuthContextProvider } from './context/auth-provider.tsx'
+import { Worker } from '@react-pdf-viewer/core'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryProvider>
     <AuthContextProvider>
-      <App />
+      <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+        <App />
+      </Worker>
     </AuthContextProvider>
   </QueryProvider>
 )
